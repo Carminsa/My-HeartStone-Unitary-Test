f53cc0350ff3f3082c477b1fa870c3ee4f19f5cc
geo <geoffrey.leborges@epitech.eu>
2017-02-19 13:03:28 +0100

    class player ok

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -2,6 +2,7 @@ import config from '../src/models/config';
 
 var players = JSON.parse(config.player.param);
 var model = new config.player.class(players);
+var player_2 = new config.player.class(players);
 
 describe('Class Player', function(){
     it('constructor est une instance de la classe deck', function(){
@@ -31,10 +32,25 @@ describe('Class Player', function(){
     });
 
     describe('DisCard', function(){
-       it('retrourne true si la carte est envoyé au cimetière', function(){
-           expect(model.discard(1));
-       });
+        it('retrourne true si la carte est envoyé au cimetière', function(){
+            expect(model.discard(1)).toBeTruthy();
+        });
+
+        it('la fonction renvoie false si erreur', function(){
+            expect(model.discard(91)).toBeFalsy();
+        });
     });
+
+    describe('Attack', function(){
+        it('la methode renvoit true si l attaqua a bien été éxécuté', function(){
+            expect(model.attack(4, player_2)).toBeTruthy();
+        });
+
+        it('la methode renvoit false si probleme', function(){
+            expect(model.attack(14, player_2)).toBeFalsy();
+        });
+    });
+
 
 
 });
--- src/models/player.js
+++ src/models/player.js
@@ -10,7 +10,7 @@ export default class Player extends Pawn {
         this.deck = ModelFactory.get('deck');
         this.cards = this.deck.cards;
 
-        // this.board = ModelFactory.get('board');
+        this.board = ModelFactory.get('board');
         this.hand = ModelFactory.get('hand');
         this.cemetary = ModelFactory.get('cemetary');
     }
@@ -45,7 +45,6 @@ export default class Player extends Pawn {
     playCard(position){
 
         var count = Object.keys(this.hand.cards).length;
-
         this.hand.cards.splice(position-1, 1);
 
         if (count -1 == Object.keys(this.hand.cards).length){
@@ -56,16 +55,30 @@ export default class Player extends Pawn {
     }
 
     discard(position){
-        var count = Object.keys(this.hand.cards).length;
+        var count_cimetary =  Object.keys(this.cemetary.cards).length;
         var cymetery = this.hand.cards[position];
         this.hand.cards.splice(position-1, 1);
         this.cemetary.cards.splice(0,1 , cymetery);
 
-        // if (count -1 == Object.keys(this.hand.cards).length){
-        //     return true;
-        // }else {
-        //     return false;
-        // }
+        if (count_cimetary +1 == Object.keys(this.cemetary.cards).length){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    attack(position, target){
+
+        var lenght_board = Object.keys(this.hand.cards).length;
+
+        if (lenght_board >= position){
+            var card = this.hand.cards[position-1];
+            return true;
+        }
+
+        if (lenght_board <= position){
+            return false;
+        }
     }
 
 
